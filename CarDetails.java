21866a8a9742efc3b1fc13fd43fdc5adaacfe1f4
Tena <tena130899@gmail.com>
2021-01-07 12:22:50 +0530

    some changes

--- CarDetails.java
+++ CarDetails.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class CarDetails {
 
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the car name:");
 		String carName = sc.nextLine();
@@ -11,6 +10,7 @@ public class CarDetails {
 		String carNo = sc.nextLine();
 		System.out.println("Enter the price:");
 		double carPrice = sc.nextDouble();
+		System.out.println("Added");
 		
 		System.out.println("Car name:"+carName);
 		System.out.println("Car no:"+carNo);
